415b17849f25dd3d940e66b1b2a1446322ced9b3
Md. Humayun Kabir <32635745+hkanik@users.noreply.github.com>
2019-09-27 02:47:51 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//for smoothscrooll
+var scroll = new SmoothScroll('a[href*="#"]', {
+	// Function. Custom easing pattern
+	// If this is set to anything other than null, will override the easing option above
+	customEasing: function (time) {
+
+		// return <your formulate with time as a multiplier>
+
+		// Example: easeInOut Quad
+		return time < 0.5 ? 2 * time * time : -1 + (4 - 2 * time) * time;
+
+	}
+});
